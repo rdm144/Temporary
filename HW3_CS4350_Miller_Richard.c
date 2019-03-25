5051ce4d486407a6c6997ede3962f91616d2f972
rdm144 <46978172+rdm144@users.noreply.github.com>
2019-03-24 22:57:44 -0500

    Update HW3_CS4350_Miller_Richard.c

--- HW3_CS4350_Miller_Richard.c
+++ HW3_CS4350_Miller_Richard.c
@@ -11,7 +11,7 @@ Due Date: 3/27/2019
 
 #include <stdio.h>
 
-void encrypt(char* message, int key)
+void encrypt(char* message, int *key)
 {
 	int capitals = 0;
 	printf("\nNumber of Capital Letters: %i", capitals);
@@ -31,7 +31,7 @@ void encrypt(char* message, int key)
 	printf("Concatenation of original and encrypted text: %s\n\n", combo);
 }
 
-void decrypt(char* message, int key)
+void decrypt(char* message, int *key)
 {
 	int capitals = 0;
 	printf("\nNumber of Capital Letters: %i", capitals);
@@ -51,17 +51,20 @@ void decrypt(char* message, int key)
 	printf("Concatenation of original and encrypted text: %s\n\n", combo);
 }
 
-int validate(int which)
+int validate(char* c, int *k, int which)
 {
 	char en[] = "encrypt";
 	char de[] = "decrypt";
 	char ex[] = "exit";
-
-	char temp;
+	int isEncrypt = 1;
+	int isDecrypt = 1;
+	int isExit = 1;
+	
 	switch (which)
 	{
 	case 1:
-		int status = scanf("%s", &choice);
+		int status = scanf("%s", &c);
+		char temp;
 		while ((temp = getchar()) != EOF && temp != '\n');
 
 		if (status != 1)
@@ -71,28 +74,34 @@ int validate(int which)
 		}
 
 		for (int i = 0; i < 7; i++)
-			if (choice[i] != en[i])
+			if (c[i] != en[i])
 				isEncrypt = 0;
 		for (int i = 0; i < 7; i++)
-			if (choice[i] != de[i])
+			if (c[i] != de[i])
 				isDecrypt = 0;
 		for (int i = 0; i < 4; i++)
-			if (choice[i] != ex[i])
+			if (c[i] != ex[i])
 				isExit = 0;
-		if (isEncrypt == 0 && isDecrypt == 0 && isExit == 0)
-			return 0;
+
+		if (isEncrypt == 1)
+			return 1;
+		else if (isDecrypt == 1)
+			return 2
+		else if (isExit == 1)
+			return 3;
 		else
-			return 1
+			return 0;
 			break;
 	case 2:
-		int status = scanf("%d", &key);
+		int status = scanf("%d", k);
+		char temp;
 		while ((temp = getchar()) != EOF && temp != '\n');
 		if (status != 1)
 		{
 			printf("Invalid message\n");
 			return 0;
 		}
-		if (key < 1 || key > 95)
+		if (k < 1 || k > 95)
 			return 0;
 		else
 			return 1;
@@ -102,52 +111,31 @@ int validate(int which)
 
 int main()
 {
-	/*
-	int input, temp, status;
-
-	printf("Please enter a number: ");
-	status = scanf("%d", &input);
-	while (status != 1) {
-	while ((temp = getchar()) != EOF && temp != '\n');
-	printf("Invalid input... please enter a number: ");
-	status = scanf("%d", &input);
-	}
-
-	printf("Your number is %d\n", input);
-	*/
-
 	int keepLooping = 1;
 	printf("Welcome to cryptography\n\n");
 	while (keepLooping == 1)
 	{
 		char choice[255];
 		char message[255];
-
-		int key;
-		int status;
-		int temp;
-		int isEncrypt;
-		int isDecrypt;
-		int isExit;
-		isEncrypt = 1;
-		isDecrypt = 1;
-		isExit = 1;
-
+		int *key;
+		int which = 0;
 		printf("What would you like to do to a message?(encrypt, decrypt, exit)\nEnter your choice: ");
-		if (validate(1) != 1)
+		which = validate(choice, key, 1);
+		if (which == 0)
 			continue;
 
 		printf("Enter your message:\n");
+		scanf("%s", &c);
 
 		printf("Enter the key number(1-95)\n");
-		if (validate(2) != 1)
+		if (validate(choice, key, 2) != 1)
 			continue;
 
-		if (isEncrypt == 1)
+		if (which == 1)
 			encrypt(message, key);
-		else if (isDecrypt == 1)
+		else if (which == 2)
 			decrypt(message, key);
-		else if (isExit == 1)
+		else if (which == 3)
 			keepLooping = 0;
 	}
 	printf("Richard Miller Security Systems\n3-27-2019");
