92ccb6120ff41422d10718b51f97d9ba6ade96ef
rdm144 <46978172+rdm144@users.noreply.github.com>
2019-03-25 23:35:19 -0500

    Add files via upload

--- hw1.py
+++ hw1.py
@@ -1,19 +1,36 @@
-#!/usr/bin/python
-import numpy
+#!/usr/bin/env python
+import numpy as np
 import pandas
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from matplotlib import pyplot as plt
 
 # load dataset
 dataframe = pandas.read_csv("ENB2012_data.csv", delimiter=',', header=None)
 dataset = dataframe.values
+
 # split into input (X) and output (Y) variables
 X = dataset[:,0:7]
 Y1 = dataset[:,8]
 Y2 = dataset[:,9]
-print("End!")
+
+# split data into training and testing data
+xTrain, xTest, yTrain, yTest = train_test_split(X, Y1, test_size = 0.3, random_state = 0)
+
+# Test the algorithm
+lm = LinearRegression()
+lm.fit(xTrain, yTrain)
+yPred = lm.predict(xTest)
+
+# Get info about algorithm test
+print('Mean Absolute Error:', metrics.mean_absolute_error(yTest, yPred))
+print('Mean Squared Error:', metrics.mean_squared_error(yTest, yPred))  
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(yTest, yPred)))
+print('Score:', lm.score(xTest, yTest))
+
+# plot values
+plt.scatter(yTest, yPred)
+plt.xlabel("Actual Values")
+plt.ylabel("Predicted Values")
+plt.show()
