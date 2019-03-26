aeb77fa02fc2cecda21f0e1783c0b993d46bb8eb
rdm144 <46978172+rdm144@users.noreply.github.com>
2019-03-25 21:15:20 -0500

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python
 import numpy
 import pandas
-import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+# load dataset
+dataframe = pandas.read_csv("ENB2012_data.csv", delimiter=',', header=None)
+dataset = dataframe.values
+# split into input (X) and output (Y) variables
+X = dataset[:,0:7]
+Y1 = dataset[:,8]
+Y2 = dataset[:,9]
+print("End!")
